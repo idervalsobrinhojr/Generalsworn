fe984c850fdbb7d88c1402e33f44f0cc8ce93d6a
idervalsobrinhojr <idervalsobrinhojr@gmail.com>
2026-07-10 12:37:27 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "actionroll-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./ActionRollPicture.png",
+    "./icon-192.png",
+    "./icon-512.png",
+    "./IMFellEnglish-Regular.ttf",
+    "./icon-maskable.png"
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(resp => resp || fetch(e.request))
+    );
+});
